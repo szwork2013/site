8aea61870bfddefa707cfae7cec11429b10c3da7
unknown <BENZ@SUMMER.(none)>
2012-03-15 10:18:02 +0800

    20120314

--- controllers/site.js
+++ controllers/site.js
@@ -9,6 +9,9 @@ var Site = models.Site;
 var Type = models.Type;
 var Combo = require('combo').Combo;
 var nginx = require('../server/nginx');
+var path = require('path');
+var fs = require('fs');
+var config = require('../config').config;
 
 exports.index = function (req, res) {
   res.render('site/index');
@@ -29,7 +32,15 @@ exports.add = function (req, res, next) {
       nginx.conf(site, function (err) {
         if (err) return next(err);
 
-        res.redirect('/site/list/' + site.type);
+        var action = nginx.status() ? 'reload' : 'start';
+        nginx.action(action, function (err) {
+          if (err) {
+            res.render('message', {status: 0, message: '站点添加成功,更新nginx时发生错误' + err});
+          }
+          else {
+            res.render('message', {status: 1, message: '站点添加成功', url: '/site/list/' + site.type});
+          }
+        });
       });
     });
   }
@@ -49,10 +60,19 @@ exports.edit = function (req, res, next) {
     Site.findById(req.body.id, function (err, site) {
       if (err) return next(err);
 
-      var modifySiteConf = false;
+      if (!site) {
+        res.render('message', {status: 0, message: '该网站不存在', url: '/site/list'});
+      }
 
-      if (site.domain !== req.body.domain || site.path !== req.body.path) {
-        modifySiteConf = true;
+      var needUpdateNginx = false;
+      var originDomain = site.domain;
+
+      if (site.domain !== req.body.domain) {
+        needUpdateNginx = true;
+      }
+
+      if (site.path !== req.body.path) {
+        needUpdateNginx = true;
       }
 
       site.name = req.body.name;
@@ -69,18 +89,30 @@ exports.edit = function (req, res, next) {
       site.save(function (err) {
         if (err) return next(err);
 
-        nginx.conf(site, function (err) {
-          if (err) return next(err);
+        if (needUpdateNginx) {
 
-          var action = nginx.status() ? 'reload' : 'start';
-          nginx.action(action, function (err) {
-            if (err) {
-              res.render('message', {status: 0, message: '站点修改成功,更新nginx时发生错误' + err});
-            }else {
-              res.render('message', {status: 1, message: '站点修改成功', url: '/site/list/' + site.type});
+          if (originDomain !== site.domain) {
+            var confpath = path.join(config.nginx.conf, originDomain) + '.conf';
+            if (path.existsSync(confpath)) {
+              fs.unlinkSync(confpath);
             }
+          }
+
+          nginx.conf(site, function (err) {
+            if (err) return next(err);
+
+            var action = nginx.status() ? 'reload' : 'start';
+            nginx.action(action, function (err) {
+              if (err) {
+                res.render('message', {status: 0, message: '站点修改成功,更新nginx时发生错误' + err});
+              } else {
+                res.render('message', {status: 1, message: '站点修改成功', url: '/site/list/' + site.type});
+              }
+            });
           });
-        });
+        } else {
+          res.render('message', {status: 1, message: '站点修改成功', url: '/site/list/' + site.type});
+        }
       });
     });
   }
@@ -111,7 +143,7 @@ exports.list = function (req, res, next) {
     res.render('site/list', {curtype: curtype, sites: sites[1], types: types[1], typeos: types[2], sitesByType: sitesByType[1]});
   });
 
-  Site.find(curtype ? {type: curtype} : {}, cb.add());
+  Site.find(curtype ? {type: curtype} : {}).desc('_id').run(cb.add());
   Type.list(cb.add());
   Type.sitesByType(cb.add());
 };
